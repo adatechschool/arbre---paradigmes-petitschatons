6ba2efd1fe7b4ea19a3dd85f6765777ff59e4243
CelineBarbe <laloutrecosmique@hotmail.fr>
2021-11-09 16:35:05 +0100

    étape 4 compatibility

--- Appliquer/script.js
+++ Appliquer/script.js
@@ -6,14 +6,17 @@ let trips = [
 ]
 var clientList = [];
 
+// extraire les voyages tout en les découpant pour en extraire chaque morceau ensuite
+// création d'un objet client pour reprendre les différentes valeurs d'un voyage
 function parseTrip(trips) {
     trips.forEach(trip => {
         let info = trip.split(" ");
         let client = {
             "client" : info[0],
-            "start" : info[1],
-            "duration" : info[2],
-            "price" : info[3]
+            "start" : parseInt(info[1]),
+            "duration" : parseInt(info[2]),
+            "end": parseInt(info[1]) + parseInt(info[2]),
+            "price" : parseInt(info[3])
          }
         clientList.push(client);
     });
@@ -25,15 +28,35 @@ console.log(dayClients);
 var dayGains = [];
 var sumGains = 0;
 
+// calcul du total des gains pour une journée en récupérant le prix facturé à chaque voyageur.
 function getTripsPrice(dayClients) {
     dayClients.forEach(dayClient => {
-        dayGains.push(parseInt(dayClient.price));
+        dayGains.push(dayClient.price);
     });
     for(let i = 0; i < dayClients.length; i++) {
-        sumGains += parseInt(dayClients[i].price);
+        sumGains += dayClients[i].price;
     }
     return sumGains;
 };
 
 getTripsPrice(dayClients); 
 console.log(sumGains);
+
+
+// vérification de la compatibilité entre deux voyages (n'empiêtent pas l'un sur l'autre)
+// en gros si end d'un voyage > start de l'autre 
+
+let tripA = {client: 'Roger', start: 0, duration: 5, end: 5, price: 10};
+let tripB = {client: 'Pongo', start: 3, duration: 7, end: 10, price: 14};
+let tripC = {client: 'Perdita', start: 8, duration: 10, end: 18, price: 8};
+let tripD = {client: 'Anita', start: 16, duration: 3, end: 19, price: 7};
+
+function checkCompatibility(tripA, tripB) {
+    if(tripA.end > tripB.start){
+        return false;
+    } else {
+        return true;
+    };
+};
+
+console.log(checkCompatibility(tripA, tripC));
